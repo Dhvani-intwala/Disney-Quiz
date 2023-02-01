d2fd2b7e8452551ed90518cf8e60fe5a49de0736
Dhvani-intwala <intdhvani2627@gmail.com>
2023-02-01 15:05:38 +0000

    updated highscore javascript

--- highscores.js
+++ highscores.js
@@ -2,7 +2,12 @@ const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 console.log(highScoresList);
-highScoresList.innerHTMl = 
+ //highScoresList.innerHTMl = 
 highScores.map(score => {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(score.name + " " +score.score));
+    li.setAttribute('class','high-score');
+    highScoresList.appendChild(li);
+    console.log(`<li class="high-score"> ${score.name}- ${score.score}</li>`);
     return `<li class="high-score"> ${score.name}- ${score.score}</li>`
 }).join('')
